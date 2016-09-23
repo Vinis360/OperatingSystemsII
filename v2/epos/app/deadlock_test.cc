25694f0e17f9f0434806b6494702923c8631345e
Thiago Barbato <thiagocbbt@gmail.com>
2016-09-23 16:47:54 -0300

    deadlock test try 4

--- v2/epos/app/deadlock_test.cc
+++ v2/epos/app/deadlock_test.cc
@@ -5,16 +5,16 @@ using namespace EPOS;
 
 OStream cout;
 
-void foo(Thread* bar) {
+void foo(Thread& bar) {
 	cout << "Locking " << bar << endl;
 	bar->join();
 }
 
 int main() {
 	Thread * a[3];
-	a[0] = new Thread(&foo, &a[1]);
-	a[1] = new Thread(&foo, &a[2]);
-	a[2] = new Thread(&foo, &a[0]);
+	a[0] = new Thread(&foo, a[1]);
+	a[1] = new Thread(&foo, a[2]);
+	a[2] = new Thread(&foo, a[0]);
 
 	a[0]->join();
 	cout << "Done" << endl;
